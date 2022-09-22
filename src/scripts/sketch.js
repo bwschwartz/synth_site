3e2a2f27a2a107d4084830f3b235968b5c534176
Benjamin Schwartz <bs4308@nyu.edu>
2022-09-22 14:54:38 -0400

    Add chain

--- src/scripts/sketch.js
+++ src/scripts/sketch.js
@@ -1,21 +1,20 @@
-//source: https://www.youtube.com/watch?v=urR596FsU68 The Coding Train
+//source: https://www.youtube.com/watch?v=urR596FsU68 "The Coding Train"
 
 const Engine = Matter.Engine,
   // Render = Matter.Render,
   World = Matter.World,
-  Bodies = Matter.Bodies;
-  Composites = Matter.Composites;
-
-
+  Bodies = Matter.Bodies,
+  Constraint = Matter.Constraint;
 
 let canvas, engine, world, testBox;
 
 let boxes = [];
 
 function windowResized(){
-  console.log('resized');
   resizeCanvas(windowWidth, windowHeight);
 }
+
+let particles = [];
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
@@ -24,29 +23,65 @@ function setup() {
 
   engine = Engine.create();
   world = engine.world;
-
-  // testBox = Bodies.rectangle(200, 100, 80, 80);
-  testBox = new Circle(200, 100, 80);
   Matter.Runner.run(engine);
-  World.add(world, testBox); //remove global variable
+
+  let isFixed = false;
+  let prev = null;
+
+  for (let x = 500; x < 1000; x+=40){
+    let isFixed = false;
+
+    if (!prev){
+      isFixed = true;
+    }
+
+    const p = new Particle(x, 100, 10, isFixed);
+    particles.push(p);
 
 
-}
+    if (prev){
+      const constraintOptions = {
+      bodyA: p.body,
+      bodyB: prev.body,
+      length: 50,
+      stiffness: .4
+    }
+
+    const constraint = Constraint.create(constraintOptions)
+    World.add(world, constraint)
+    }
+
+    prev = p;
+
+  }
+
+  // const p2 = new Particle(150, 150, 100, isFixed);
+
+
+
+
+  // particles.push(p1);
+  // particles.push(p2);
+  }
+
 
 function draw() {
-  background(100);
-  // rect(testBox.position.x, testBox.position.y, 80, 80);
-  testBox.show();
+  background('transparent');
+  for (i=0; i<particles.length; i++){
+    particles[i].show();
+  }
+
 }
 
-function mousePressed() {
-  boxes.push(Bodies.rectangle(200, 100, 80, 80));
-}
+function Particle(x, y, r, isFixed) {
+  let particleOptions = {
+    friction: 0,
+    restitution: 0.95,
+    isStatic: isFixed
+  }
 
-function Circle(x, y, r) {
-  this.body = Bodies.circle(x,y, r);
-  // this.w  = w;
-  // this.h = h;
+  this.body = Bodies.circle(x,y, r, particleOptions);
+
   World.add(world, this.body);
 
   this.remove = function() {
@@ -65,4 +100,10 @@ function Circle(x, y, r) {
     ellipse(0, 0, r);
     pop();
   }
+}
+
+
+
+function mousePressed() {
+  boxes.push(Bodies.rectangle(200, 100, 80, 80));
 }
