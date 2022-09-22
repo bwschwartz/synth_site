329c03c3bcaebbbb02f3e9339e457f1c65fe0277
Benjamin Schwartz <bs4308@nyu.edu>
2022-09-22 07:56:13 -0400

    Add color coding

--- src/scripts/connection.js
+++ src/scripts/connection.js
@@ -1,22 +1,47 @@
 class Connection {
   constructor(audioCtx, oscArray, ampKnobArray) {
-    this.params = null;
     this.audioCtx = audioCtx;
     this.oscArray = oscArray;
+    this.ampKnobArray = ampKnobArray;
+    this.params = null;
     this.sender = null;
     this.receiver = null;
-    this.ampKnobArray = ampKnobArray;
     this.color = null;
+    this.colors = ['cornsilk', 'paleogoldenrod', 'aliceblue', 'firebrick', 'azure', 'bisque'];
   }
 
   init() {
-    // const testOut = document.querySelector(`#sin-out1`);
-    // testOut.addEventListener('mousedown', this.sendParams.bind(this));
-
-    // const testIn  = document.querySelector(`#amp-in1`)
-    // testIn.addEventListener('mousedown', this.receiveParams.bind(this))
-
     this.setUpEventListeners();
+  }
+
+  sendParams(e) {
+    this.sender = e.target.id;
+
+    this.params = {
+      waveform: this.sender.slice(0, 3),
+      oscNum: this.sender.slice(this.sender.length-1) - 1
+    };
+
+    this.color = this.colors[ Math.floor( Math.random() * 8 ) ]; // need func to add/remove colors from array
+    console.log(this.sender)
+    document.querySelector(`#${this.sender}`).style.backgroundColor = this.color;
+
+    this.sender = null;
+  }
+
+  receiveParams(e) {
+    if (this.params){
+      const osc = this.oscArray[this.params['oscNum']];
+
+      const ampKnobNum = e.target.id.slice(e.target.id.length-1) - 1;
+      const ampKnob = this.ampKnobArray[ampKnobNum];
+
+      ampKnob.updateOscillator(osc, this.params['waveform']);
+
+      this.params = null;
+
+      document.querySelector(`#${e.target.id}`).style.backgroundColor = this.color;
+    }
   }
 
   setUpEventListeners() {
@@ -30,51 +55,11 @@ class Connection {
     document.querySelector(`#sqr-out3`).addEventListener('mousedown', this.sendParams.bind(this));
     document.querySelector(`#sqr-out4`).addEventListener('mousedown', this.sendParams.bind(this));
 
-
     document.querySelector(`#amp-in1`).addEventListener('mousedown', this.receiveParams.bind(this));
     document.querySelector(`#amp-in2`).addEventListener('mousedown', this.receiveParams.bind(this));
-    // document.querySelector(`#amp-in3`).addEventListener('mousedown', this.receiveParams.bind(this));
-    // document.querySelector(`#amp-in4`).addEventListener('mousedown', this.receiveParams.bind(this));
-
-
-
-
+    document.querySelector(`#amp-in3`).addEventListener('mousedown', this.receiveParams.bind(this));
+    document.querySelector(`#amp-in4`).addEventListener('mousedown', this.receiveParams.bind(this));
   }
-
-  sendParams(e) {
-    this.sender = e.target.id;
-    // console.log(this.sender);
-    this.params = {
-      waveform: this.sender.slice(0, 3),
-      oscNum: this.sender.slice(this.sender.length-1) - 1
-    };
-    this.sender = null;
-    this.color = 'blue';
-
-    document.querySelector(`#${e.target.id}`).style.backgroundColor = 'black';
-  }
-
-  receiveParams(e) {
-    // console.log(`receive params: ${this.params['waveform']}`)
-    if (this.params){
-      const osc = this.oscArray[this.params['oscNum']]
-      // console.log(`receiving oscillator: ${osc}`);
-
-      const ampKnobNum = e.target.id.slice(e.target.id.length-1) - 1;
-      const ampKnob = this.ampKnobArray[ampKnobNum];
-
-      // const waveform = this.params['waveform'];
-      ampKnob.updateOscillator(osc, this.params['waveform']);
-
-      this.params = null;
-
-      document.querySelector(`#${e.target.id}`).style.backgroundColor = 'black';
-
-    }
-  }
-
-
-
 }
 
 module.exports = Connection;
