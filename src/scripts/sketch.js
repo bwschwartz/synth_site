f15df74641bb0577cc52131da52caefb4af30c7c
Benjamin Schwartz <bs4308@nyu.edu>
2023-01-03 09:31:01 -0500

    Make cables sticky to both sides

--- src/scripts/sketch.js
+++ src/scripts/sketch.js
@@ -29,32 +29,32 @@ function setup() {
   let isFixed = false;
   let prev = null;
 
-  const position = 1500
+  // const position = 1500
 
-  for (let x = position; x < position +100; x+=10){
-    let isFixed = false;
+  // for (let x = position; x < position +100; x+=10){
+  //   let isFixed = false;
 
-    if (!prev){
-      isFixed = true;
-    }
+  //   if (!prev){
+  //     isFixed = true;
+  //   }
 
-    const p = new Particle(x, 300, 30, isFixed);
-    // debugger;
-    particles.push(p);
+  //   const p = new Particle(x, 300, 30, isFixed);
+  //   // debugger;
+  //   particles.push(p);
 
-    if (prev){
-      const constraintOptions = {
-      bodyA: p.body,
-      bodyB: prev.body,
-      length: 100,
-      stiffness: 1.0
-    }
+  //   if (prev){
+  //     const constraintOptions = {
+  //     bodyA: p.body,
+  //     bodyB: prev.body,
+  //     length: 100,
+  //     stiffness: 1.0
+  //   }
 
-    const constraint = Constraint.create(constraintOptions)
-    World.add(world, constraint)
-    }
-    prev = p;
-    }
+  //   const constraint = Constraint.create(constraintOptions)
+  //   World.add(world, constraint)
+  //   }
+  //   prev = p;
+  //   }
 
 
     const mouse = Mouse.create( canvas.elt );
@@ -74,15 +74,13 @@ function draw() {
   // background('rgba(10,10,100, 0)'); // effect
   background('rgba(10,10,100, 0)'); // effect
 
-
-  // background('yellow');
+  background('palegoldenrod');
   for (i=0; i<particles.length; i++){
     particles[i].show();
-
   }
 }
 
-function Particle(x, y, r, isFixed) {
+function Particle(x, y, r, isFixed, hole, end) {
   let particleOptions = {
     friction: 0,
     restitution: .2,
@@ -108,7 +106,9 @@ function Particle(x, y, r, isFixed) {
     rectMode(CENTER);
     ellipse(0, 0, r);
     pop();
-  }
+  },
+  this.hole = (hole || null )
+  this.end = end;
 }
 
 function mouseDown(e) {
@@ -116,8 +116,75 @@ function mouseDown(e) {
   // e.stopPropagation();
 }
 
+function mousePressed(e) {
+  console.log("mosue pressed")
+  console.log("e is ", e)
 
+  if (particles[0] && e.target.classList[1] === "input") {
+    console.log("in input")
+    particles[particles.length-1].body.position.x = e.clientX
+    particles[particles.length-1].body.position.y = e.clientY
+    particles[particles.length-1].body.isStatic = true;
+    particles[particles.length-1].hole = e.target.id;
+    document.querySelector(`#${e.target.id}`).getBoundingClientRect().x
+    console.log("partciles in pressed are", particles)
+  }
+
+  if (e.target.classList[0] === "hole" && e.target.classList[1] !== "input") {
+    let isFixed = false;
+    let prev = null;
+
+
+    const  xPos = e.clientX;
+    const yPos = e.clientY;
+
+    for (let x = xPos; x < xPos +100; x+=10){
+      let hole = null;
+      let isFixed = false;
+      if (x === xPos ) {
+        hole = e.target.id
+      }
+
+      if (!prev){
+        isFixed = true;
+      }
+
+      const p = new Particle(x, yPos, 30, isFixed, hole);
+      particles.push(p);
+
+      if (prev){
+        const constraintOptions = {
+        bodyA: p.body,
+        bodyB: prev.body,
+        length: 100,
+        stiffness: 1.0
+      }
+
+      const constraint = Constraint.create(constraintOptions)
+      World.add(world, constraint)
+      }
+      prev = p;
+      }
+    }
+
+}
 
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
+}
+
+function mouseDragged(e){
+  for (let i=0; i<particles.length; i++) {
+    if (particles[i].hole) {
+      particles[i].body.position.x = document.querySelector(`#${particles[i].hole}`).getBoundingClientRect().x
+      particles[i].body.position.y = document.querySelector(`#${particles[i].hole}`).getBoundingClientRect().y
+
+      // particles[i].body.position.y =  document.querySelector(`#${particles[i].hole}`).clientY
+
+
+    }
+
+    // console.log("e.offsetX is", e)
+  }
+
 }
